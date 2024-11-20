1310a0ce7642310162306994938b9f4978b6d02b
Fawaz <aljawif1@gmail.com>
2024-11-21 01:34:29 +0700

    Add

---
+++ DOM manipulation/script.js
@@ -0,0 +1,53 @@
+// const jdl = document.getElementById("judul");
+// jdl.innerHTML = "<em>Fawaz</em>";
+
+// const sct = document.querySelector("section#b");
+// sct.innerHTML = "<b>Hello world</b>";
+
+// element style
+// const judul = document.querySelector("#judul");
+// judul.style.color = "purple";
+// judul.style.backgroundColor = "orange";
+
+// element attribute
+// getAttribute | setAttribute | removeAttribute
+// const judul = document.getElementsByTagName("h1")[0];
+// judul.setAttribute("class", "Fawaz");
+// const a = document.querySelector("section#a a");
+// a.setAttribute("id", "link");
+
+// jika igin mengetahui atribut href dari a pada console
+//  a.getAttribute('href')
+// jika judul maka
+//  judul.getAttribute("id")
+// jika ingin menghapus atributnya maka
+//  a.removeAttribute("href")
+
+// elemnt classlist
+// const p2 = document.querySelector(".p2");
+
+// add
+// p2.classList.add("label");
+
+// remove
+// p2.classList.remove("label");
+
+// toggle
+// p2.classList.toggle("label2"); // true
+// p2.classList.toggle("label2"); // false
+// contoh lain
+// document.body.classList.toggle("biru-muda"); // biru-muda adalah class dari css
+// document.body.classList.toggle("biru-muda");
+
+// element item
+// p2.classList.add("satu", "dua", "tiga");
+// bisa coba di console browser
+// p2.classList.item(2); // sesuai urutan dengan index dari 0
+
+// element contain -> untuk tanya apakah ada atribut didalamnya
+// p2.classList.contains("dua"); // jika ada maka true
+// p2.classList.contains("lima"); // jika tidak ada maka false
+
+// element replace
+// p2.classList.replace("dua", "empat");
+// p2.classList.replace("tiga", "lima");
